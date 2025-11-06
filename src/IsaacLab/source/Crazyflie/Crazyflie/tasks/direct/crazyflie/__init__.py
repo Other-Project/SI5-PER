b727ce5257c45dce36e1bf5d4ac93e7c0434f7fa
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-11-06 17:01:53 +0100

    fix: class entry point instead of file

--- src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/__init__.py
+++ src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/__init__.py
@@ -19,7 +19,7 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.crazyflie_env:CrazyflieEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:QuadcopterPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
-        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
     },
 )
