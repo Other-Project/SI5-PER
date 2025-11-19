7654820d8a68ec29d1986f0d15b3b77c7872a3c2
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-11-19 17:04:47 +0100

    feat: moving goal

--- src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/crazyflie_env.py
+++ src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/crazyflie_env.py
@@ -17,13 +17,13 @@ from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.terrains import TerrainImporterCfg
 from isaaclab.utils import configclass
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from isaaclab.utils.math import subtract_frame_transforms
-from isaaclab_assets import ANYDRIVE_3_SIMPLE_ACTUATOR_CFG  # isort: skip
 ##
 # Pre-defined configs
 ##
 from isaaclab_assets import CRAZYFLIE_CFG  # isort: skip
+
+from ....assets import ALPHABOT_CFG, ALPHABOT_JOINTS_NAMES, ACTUATORS_LEFT_WHEEL, ACTUATORS_RIGHT_WHEEL
 
 
 class CrazyflieEnvWindow(BaseEnvWindow):
@@ -94,29 +94,10 @@ class CrazyflieEnvCfg(DirectRLEnvCfg):
     thrust_to_weight = 1.9
     moment_scale = 0.01
 
-    platform: ArticulationCfg = ArticulationCfg(
-        prim_path="/World/envs/env_.*/Platform",
-        spawn=sim_utils.UsdFileCfg(
-            usd_path=f"{ISAAC_NUCLEUS_DIR}/Samples/ROS2/Robots/turtlebot3_burger_ROS.usd",
-            scale=(1.0, 1.0, 1.0),
-        ),
-        init_state=ArticulationCfg.InitialStateCfg(
-            pos=(0.0, 0.0, 0.0),
-            rot=(1.0, 0.0, 0.0, 0.0),
-        ),
-        actuators={}
-    )
-
     # reward scales
     lin_vel_reward_scale = -0.05
     ang_vel_reward_scale = -0.01
     distance_to_goal_reward_scale = 15.0
-
-    # random pose range
-    platform_spawn_range_xy = 1.0
-    platform_spawn_z = 0.0
-    drone_min_height = 0.4
-    drone_max_height = 0.6
 
 class CrazyflieEnv(DirectRLEnv):
     cfg: CrazyflieEnvCfg
@@ -151,9 +132,7 @@ class CrazyflieEnv(DirectRLEnv):
 
     def _setup_scene(self):
         self._robot = Articulation(self.cfg.robot)
-        self._platform = Articulation(self.cfg.platform)
         self.scene.articulations["robot"] = self._robot
-        self.scene.articulations["platform"] = self._platform
 
         self.cfg.terrain.num_envs = self.scene.cfg.num_envs
         self.cfg.terrain.env_spacing = self.scene.cfg.env_spacing
@@ -168,6 +147,8 @@ class CrazyflieEnv(DirectRLEnv):
         light_cfg.func("/World/Light", light_cfg)
 
     def _pre_physics_step(self, actions: torch.Tensor):
+        self._update_moving_goal()
+
         self._actions = actions.clone().clamp(-1.0, 1.0)
         self._thrust[:, 0, 2] = self.cfg.thrust_to_weight * self._robot_weight * (self._actions[:, 0] + 1.0) / 2.0
         self._moment[:, 0, :] = self.cfg.moment_scale * self._actions[:, 1:]
@@ -176,7 +157,6 @@ class CrazyflieEnv(DirectRLEnv):
         self._robot.set_external_force_and_torque(self._thrust, self._moment, body_ids=self._body_id)
 
     def _get_observations(self) -> dict:
-        self._desired_pos_w = self._platform.data.root_pos_w.clone()
         desired_pos_b, _ = subtract_frame_transforms(
             self._robot.data.root_pos_w, self._robot.data.root_quat_w, self._desired_pos_w
         )
@@ -234,17 +214,6 @@ class CrazyflieEnv(DirectRLEnv):
         extras["Metrics/final_distance_to_goal"] = final_distance_to_goal.item()
         self.extras["log"].update(extras)
 
-        num_envs_to_reset = len(env_ids)
-
-        random_xy = (torch.rand(num_envs_to_reset, 2,
-                                device=self.device) - 0.5) * 2.0 * self.cfg.platform_spawn_range_xy
-        random_z = torch.full((num_envs_to_reset, 1), self.cfg.platform_spawn_z, device=self.device)
-        random_pos = torch.cat([random_xy, random_z], dim=-1)
-        random_pos += self._terrain.env_origins[env_ids]
-        default_root_state_platform = self._platform.data.default_root_state[env_ids]
-        default_root_state_platform[:, :3] = random_pos
-        self._platform.write_root_pose_to_sim(default_root_state_platform[:, :7], env_ids)
-
         self._robot.reset(env_ids)
         super()._reset_idx(env_ids)
         if len(env_ids) == self.num_envs:
@@ -253,17 +222,32 @@ class CrazyflieEnv(DirectRLEnv):
 
         self._actions[env_ids] = 0.0
         # Sample new commands
-        self._desired_pos_w[env_ids, :] = self._platform.data.root_pos_w[env_ids, :]
+        self._desired_pos_w[env_ids, :2] = (
+                torch.zeros(len(env_ids), 2, device=self.device).uniform_(-1.0, 1.0)
+                + self._terrain.env_origins[env_ids, :2]
+        )
+        self._desired_pos_w[env_ids, 2] = torch.ones(len(env_ids), device=self.device) * 1.0
+
+        rand_xy_offset = torch.zeros(len(env_ids), 2, device=self.device).uniform_(-2.5, 2.5)
+        rand_z = torch.zeros(len(env_ids), 1, device=self.device).uniform_(2.5, 3.5)
+
+        start_pos = torch.cat([self._desired_pos_w[env_ids, :2] + rand_xy_offset, rand_z], dim=1)
+
+        default_root_state = self._robot.data.default_root_state[env_ids]
+        default_root_state[:, :3] = start_pos
+
         # Reset robot state
         joint_pos = self._robot.data.default_joint_pos[env_ids]
         joint_vel = self._robot.data.default_joint_vel[env_ids]
         default_root_state = self._robot.data.default_root_state[env_ids]
-        default_root_state[:, 2] = (torch.rand(len(env_ids), device=self.device) * (
-                self.cfg.drone_max_height - self.cfg.drone_min_height) + self.cfg.drone_min_height)
         default_root_state[:, :3] += self._terrain.env_origins[env_ids]
         self._robot.write_root_pose_to_sim(default_root_state[:, :7], env_ids)
         self._robot.write_root_velocity_to_sim(default_root_state[:, 7:], env_ids)
         self._robot.write_joint_state_to_sim(joint_pos, joint_vel, None, env_ids)
+
+    def _update_moving_goal(self):
+        speed = 0.05
+        self._desired_pos_w[:, 0] += speed * self.step_dt
 
     def _set_debug_vis_impl(self, debug_vis: bool):
         # create markers if necessary for the first time
