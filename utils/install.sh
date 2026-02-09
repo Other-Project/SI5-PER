1ce11652a423fece00c0147e6cc8a4fae9052dc3
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2026-02-09 11:50:04 +0100

    chore: Add webots to install script

--- utils/install.sh
+++ utils/install.sh
@@ -8,31 +8,30 @@
 
 
 # Enable universe
-sudo apt install software-properties-common -y
+sudo apt install software-properties-common curl lsb-release gnupg -y
 sudo add-apt-repository universe
+sudo apt update -y
 
-# Add ROS source
-sudo apt update -y && sudo apt install curl -y
+# Add sources
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
 sudo dpkg -i /tmp/ros2-apt-source.deb
-sudo apt update -y
-sudo apt upgrade -y
 
-# Install ROS
-sudo apt install ros-jazzy-desktop -y
+sudo mkdir -p /etc/apt/keyrings
+cd /etc/apt/keyrings
+sudo wget -q https://cyberbotics.com/Cyberbotics.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/Cyberbotics.asc] https://cyberbotics.com/debian binary-amd64/" | sudo tee /etc/apt/sources.list.d/Cyberbotics.list
 
-# Add Gazebo source
-sudo apt-get install lsb-release gnupg -y
 sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] https://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] https://packages.osrfoundation.org/gazebo/ubuntu-prerelease $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-prerelease.list > /dev/null
-sudo apt-get update -y
 
-# Install Gazebo
-sudo apt-get install gz-harmonic -y
+# Update packages
+sudo apt update -y
+sudo apt upgrade -y
 
-# Install ROS packages
-sudo apt install libboost-program-options-dev libusb-1.0-0-dev python3-colcon-common-extensions -y
-sudo apt install ros-jazzy-motion-capture-tracking ros-jazzy-tf-transformations ros-jazzy-teleop-twist-keyboard -y
-sudo apt install ros-jazzy-ros-gz -y
+# Install packages
+sudo apt install gz-harmonic webots \
+    ros-jazzy-desktop ros-jazzy-ros-gz \
+    libboost-program-options-dev libusb-1.0-0-dev python3-colcon-common-extensions \
+    ros-jazzy-motion-capture-tracking ros-jazzy-tf-transformations ros-jazzy-teleop-twist-keyboard -y
