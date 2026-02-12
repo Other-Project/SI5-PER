008f0ee6314cb05012d28731ab371fb2d63c2b58
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2026-02-12 18:07:29 +0100

    chore: change model parameters

--- src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/agents/rsl_rl_ppo_cfg.py
+++ src/IsaacLab/source/Crazyflie/Crazyflie/tasks/direct/crazyflie/agents/rsl_rl_ppo_cfg.py
@@ -10,23 +10,23 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 
 @configclass
 class QuadcopterPPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 24
-    max_iterations = 200
+    num_steps_per_env = 48
+    max_iterations = 400
     save_interval = 50
     experiment_name = "quadcopter_direct"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
-        actor_obs_normalization=False,
-        critic_obs_normalization=False,
-        actor_hidden_dims=[64, 64],
-        critic_hidden_dims=[64, 64],
+        actor_obs_normalization=True,
+        critic_obs_normalization=True,
+        actor_hidden_dims=[256, 128, 64],
+        critic_hidden_dims=[256, 128, 64],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.0,
+        entropy_coef=0.01,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=5.0e-4,
